138b687682f1bcecea49ffe6c13fd600f9da5b35
kiomarsrezai@gmail.com <kiomarsrezai@gmail.com>
2023-05-15 18:42:55 +0330

    update budgetcodingmodal1_Action in budgetapi_4

--- NewsWebsite/Areas/Api/Controllers/v1/BudgetApiController.cs
+++ NewsWebsite/Areas/Api/Controllers/v1/BudgetApiController.cs
@@ -315,7 +315,7 @@ namespace NewsWebsite.Areas.Api.Controllers.v1
             List<BudgetAreaModalViewModel> fecth = new List<BudgetAreaModalViewModel>();
             using (SqlConnection sqlconnect = new SqlConnection(_config.GetConnectionString("SqlErp")))
             {
-                using (SqlCommand sqlCommand = new SqlCommand("SP001_BudgetModal3CodingCodingArea_Read", sqlconnect))
+                using (SqlCommand sqlCommand = new SqlCommand("SP001_BudgetModal3CodingProjectArea_Read", sqlconnect))
                 {
                     sqlconnect.Open();
                     sqlCommand.Parameters.AddWithValue("AreaId", paramModel.AreaId);
