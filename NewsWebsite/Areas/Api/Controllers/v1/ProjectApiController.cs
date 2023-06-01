65da1573a6a8c673aefd814e3578c9cca03fb8e3
kiomarsrezai@gmail.com <kiomarsrezai@gmail.com>
2023-06-01 15:27:58 +0330

    ProjectReportScale fix

--- NewsWebsite/Areas/Api/Controllers/v1/ProjectApiController.cs
+++ NewsWebsite/Areas/Api/Controllers/v1/ProjectApiController.cs
@@ -345,7 +345,7 @@ namespace NewsWebsite.Areas.Api.Controllers.v1
             {
                 using (SqlConnection sqlconnect = new SqlConnection(_config.GetConnectionString("SqlErp")))
                 {
-                    using (SqlCommand sqlCommand = new SqlCommand("SP005_Commite_Modal", sqlconnect))
+                    using (SqlCommand sqlCommand = new SqlCommand("SP005_Report_ProjectScale", sqlconnect))
                     {
                         sqlconnect.Open();
                         sqlCommand.Parameters.AddWithValue("YearId", yearId);
