707191247f8dc8c06ca0284bba5071e260f9b171
mansour ghasemi <manghasemi@amcso.org>
2023-06-14 17:48:36 +0330

    edit programoperation

--- NewsWebsite/Areas/Api/Controllers/v1/ProgramApiController.cs
+++ NewsWebsite/Areas/Api/Controllers/v1/ProgramApiController.cs
@@ -6,6 +6,7 @@ using NewsWebsite.Common.Api;
 using NewsWebsite.Common.Api.Attributes;
 using NewsWebsite.Data.Contracts;
 using NewsWebsite.ViewModels.Api.Budget.BudgetProject;
+using NewsWebsite.ViewModels.Api.Public;
 using NewsWebsite.ViewModels.Program;
 using System.Collections.Generic;
 using System.Data;
@@ -55,6 +56,7 @@ namespace NewsWebsite.Areas.Api.Controllers.v1
 
         }
 
+
         [Route("ProgramOperation")]
         [HttpGet]
         public async Task<ApiResult<List<ProgramOperationViewModel>>> ProgramOperation(int ProgramId, int areaId)
@@ -86,26 +88,61 @@ namespace NewsWebsite.Areas.Api.Controllers.v1
             return Ok(fecthkol);
         }
 
+
         [Route("ProgramOperationUpdate")]
         [HttpPost]
-        public async Task<ApiResult<string>> ProgramOperationUpdate([FromBody] ProgramOperationUpdateViewModel programOperationUpdate)
+        public async Task<ApiResult<string>> ProgramOperationUpdate([FromBody] ProgramOperationUpdateViewModel param)
         {
-                using (SqlConnection sqlconnect = new SqlConnection(_config.GetConnectionString("SqlErp")))
+            string readercount = null;
+            using (SqlConnection sqlconnect = new SqlConnection(_config.GetConnectionString("SqlErp")))
                 {
                     using (SqlCommand sqlCommand = new SqlCommand("SP005_ProgramOperation_Update", sqlconnect))
                     {
                         sqlconnect.Open();
-                        sqlCommand.Parameters.AddWithValue("ProjectId", programOperationUpdate.ProjectId);
-                        sqlCommand.Parameters.AddWithValue("ScaleId", programOperationUpdate.ScaleId);
-                        sqlCommand.Parameters.AddWithValue("ProjectName", programOperationUpdate.ProjectName);
+                        sqlCommand.Parameters.AddWithValue("ProjectId", param.ProjectId);
+                        sqlCommand.Parameters.AddWithValue("ScaleId", param.ScaleId);
+                        sqlCommand.Parameters.AddWithValue("ProjectName", param.ProjectName);
                         sqlCommand.CommandType = CommandType.StoredProcedure;
                         SqlDataReader dataReader = await sqlCommand.ExecuteReaderAsync();
 
+                    while (dataReader.Read())
+                    {
+                        if (dataReader["Message_DB"].ToString() != null) readercount = dataReader["Message_DB"].ToString();
                     }
                 }
-            return Ok("بروزرسانی انجام شد");
+            }
+            if (string.IsNullOrEmpty(readercount)) return Ok("با موفقیت انجام شد");
+            else
+                return BadRequest(readercount);
         }
+
+
+        [Route("ProgramOperationDelete")]
+        [HttpPost]
+        public async Task<ApiResult<string>> Ac_ProgramOperationDelete([FromBody] DeletePublicParamViewModel param)
+        {
+            string readercount = null;
+            using (SqlConnection sqlconnect = new SqlConnection(_config.GetConnectionString("SqlErp")))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("SP005_ProgramOperation_Delete", sqlconnect))
+                {
+                    sqlconnect.Open();
+                    sqlCommand.Parameters.AddWithValue("Id", param.Id);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dataReader = await sqlCommand.ExecuteReaderAsync();
+                    while (dataReader.Read())
+                    {
+                        if (dataReader["Message_DB"].ToString() != null) readercount = dataReader["Message_DB"].ToString();
+                    }
+                }
+            }
+            if (string.IsNullOrEmpty(readercount)) return Ok("با موفقیت انجام شد");
+            else
+                return BadRequest(readercount);
+        }
+
 
     }
 
 }
+
