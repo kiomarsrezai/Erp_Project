943632651bc6cfad0ad131b6b8f4843247ff3b91
mansour ghasemi <manghasemi@amcso.org>
2023-07-03 16:25:56 +0330

    RequestContractModal

---
+++ NewsWebsite.ViewModels/Api/Request/RequestContractReadViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsWebsite.ViewModels.Api.Request
+{
+    public class RequestContractReadViewModel
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string Date { get; set; }
+        public string DateShamsi { get; set; }
+        public string Description { get; set; }
+        public string SuppliersName { get; set; }
+    }
+}
+
--- NewsWebsite/Areas/Api/Controllers/v1/RequestApiController.cs
+++ NewsWebsite/Areas/Api/Controllers/v1/RequestApiController.cs
@@ -541,9 +541,37 @@ namespace NewsWebsite.Areas.Api.Controllers.v1
             }
             return Ok();
         }
-   
-    
-    
-    
+
+
+        [Route("RequestContractRead")]
+        [HttpGet]
+        public async Task<ApiResult<List<RequestContractReadViewModel>>> AC_RequestContractRead(PublicParamIdViewModel param)
+        {
+            List<RequestContractReadViewModel> requestsViewModels = new List<RequestContractReadViewModel>();
+
+            using (SqlConnection sqlconnect = new SqlConnection(_config.GetConnectionString("SqlErp")))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("SP010_RequestContract_Read", sqlconnect))
+                {
+                    sqlconnect.Open();
+                    sqlCommand.Parameters.AddWithValue("Id", param.Id);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dataReader = await sqlCommand.ExecuteReaderAsync();
+                    while (dataReader.Read())
+                    {
+                        RequestContractReadViewModel request = new RequestContractReadViewModel();
+                        request.Id = int.Parse(dataReader["Id"].ToString());
+                        request.Number = dataReader["Number"].ToString();
+                        request.Date = dataReader["Date"].ToString();
+                        request.DateShamsi = DateTimeExtensions.ConvertMiladiToShamsi(DateTime.Parse(dataReader["Date"].ToString()), "yyyy/MM/dd");
+                        request.Description = dataReader["Description"].ToString();
+                        request.SuppliersName = dataReader["SuppliersName"].ToString();
+                        requestsViewModels.Add(request);
+                    }
+                }
+            }
+            return Ok(requestsViewModels);
+        }
+
     }
 }
