e85496958ddd6d5374f039b1ccb7a26fd20f7ebf
mansour ghasemi <manghasemi@amcso.org>
2023-08-04 11:25:07 +0330

    AmlakInfo and AmlakPrivate

---
+++ NewsWebsite.ViewModels/Api/Contract/AmlakInfoPrivateReadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsWebsite.ViewModels.Api.Contract
+{
+    public class AmlakInfoPrivateReadViewModel
+    {
+        public int Id { get; set; }
+        public string EstateInfoName { get; set; }
+        public string EstateInfoAddress { get; set; }
+    }
+}
---
+++ NewsWebsite.ViewModels/Api/Contract/ContractInstallmentsAutomaticInsertViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsWebsite.ViewModels.Api.Contract
+{
+    public class ContractInstallmentsAutomaticInsertViewModel
+    {
+        public int ContractId { get; set; }
+        public string Date { get; set; }
+        public Int64 Amount { get; set; }
+        public int Month { get; set; }
+        public int YearName { get; set; }
+    }
+}
