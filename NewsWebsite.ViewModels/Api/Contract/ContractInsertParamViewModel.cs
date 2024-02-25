3ff8f12babd379cfba05cc3c3d48e3b3efe8d037
kiomarsrezai <kiomarsrezai@gmail.com>
2024-02-25 21:30:31 +0330

    ContractAmlak

--- NewsWebsite.ViewModels/Api/Contract/ContractInsertParamViewModel.cs
+++ NewsWebsite.ViewModels/Api/Contract/ContractInsertParamViewModel.cs
@@ -17,17 +17,7 @@ namespace NewsWebsite.ViewModels.Api.Contract
         public Int64 Amount { get; set; }
      }
 
-
-    public class ContractAfterInsertViewModel
-    {
-        public int Id { get; set; }
-        public string Number { get; set; }
-        public string Date { get; set; }
-        public string Description { get; set; }
-        public int SuppliersId { get; set; }
-        public string DateFrom { get; set; }
-        public string DateEnd { get; set; }
-        public Int64 Amount { get; set; }
-    }
+    
+        
 
 }
